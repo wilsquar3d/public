d19f2363b66358f3e03ae39de81c66cfacfd45be
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2020-03-03 07:45:08 -0500

    Update utils.js

--- userscripts/utils.js
+++ userscripts/utils.js
@@ -1,3 +1,5 @@
+//https://raw.githubusercontent.com/wilsquar3d/public/master/userscripts/utils.js
+
 function createID( appID, varName, extras=[] )
 {
     return 'ww_' + appID + '_' + varName + ( extras && extras.length > 0 ? ( '_' + extras.join( '_' ) ) : '' ) + '_ww';
