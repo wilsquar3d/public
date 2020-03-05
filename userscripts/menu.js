a3dd7da2dfb4cc2ebc13a399f54adb844fb19bba
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2020-03-05 13:17:33 -0500

    Update menu.js

--- userscripts/menu.js
+++ userscripts/menu.js
@@ -1,24 +1,37 @@
 //https://raw.githubusercontent.com/wilsquar3d/public/master/userscripts/menu.js
 //requires utils.js
 
-cssBodyDefault = { padding: 0, margin: 0 };
-cssMenuItemsDefault = { padding: '10px' };
-cssMenuDefault = { height: '100%', width: '200px', padding: 0, margin: 0, float: 'left' };
-cssDisplayDefault = { height: '100%', width: 'calc(100% - 202px)', padding: 0, margin: 0, 'border-left': '2px #000 solid', float: 'left' };
-
+var global_defaults =
+    {
+        css:
+        {
+            //menu
+            body: { padding: 0, margin: 0 },
+            menuItems: { padding: '10px' },
+            menu: { height: '100%', width: '200px', padding: 0, margin: 0, float: 'left' },
+            display: { height: '100%', width: 'calc(100% - 202px)', padding: 0, margin: 0, 'border-left': '2px #000 solid', float: 'left' },
+            //menuItem
+            menuItem: { cursor: 'pointer', 'margin-bottom': '10px' },
+            menuItemHover: { on: { 'background-color': '#CCC' }, off: { 'background-color': '#FFF' } },
+            //jsonEditBox
+            editBoxHover: { on: { 'background-color': '#EEE' }, off: { 'background-color': '#FFF' } }
+        },
+        json:
+        {
+            settings: { indent: '  ' }
+        }
+    };
+/*
 function createMenu( menuItems=[], displayCSS={}, menuItemsCSS={}, menuCSS={}, bodyCSS={} )
 {
     $( 'body' ).html( '' ).css( bodyCSS );
     $( 'body' ).append(
         [
-            $( '<div id="menu" ).css( menuCSS ).append( $( '<div />' ).css( menuItemsCSS ).append( menuItems ) ),
+            $( '<div id="menu" />' ).css( menuCSS ).append( $( '<div />' ).css( menuItemsCSS ).append( menuItems ) ),
             $( '<div id="display" />' ).css( displayCSS )
         ]
     );
 }
-
-cssMenuItemDefault = { cursor: 'pointer' 'margin-bottom': '10px' };
-cssMenuItemHoverDefault = { on: { 'background-color': '#CCC' }, off: { 'background-color': '#FFF' } };
 
 //displayFunc will accept the selector to attach to
 function menuItem( name, displayFunc, css={}, hover={} )
@@ -29,9 +42,6 @@ function menuItem( name, displayFunc, css={}, hover={} )
         .text( name )
         .click( function(){ $( '#display' ).html( '' ); displayFunc( '#display' ); } );
 }
-
-jsonFormatDefault = { format: true, indent: '  ' };
-cssEditBoxHoverDefault = { on: { 'background-color': '#EEE' }, off: { 'background-color': '#FFF' } };
 
 function jsonEditBoxGM( gmVar, jsonFormat={}, hover={} )
 {
@@ -44,7 +54,7 @@ function jsonEditBoxGM( gmVar, jsonFormat={}, hover={} )
         [
             $( '<div style="padding:10px;" />' ).append(
                 $( '<textarea id="' + id + '" style="width:100%;height:calc(100% - 50px);white-space:nowrap;" />' )
-                    .val( JSON.stringify( GM_getValue( gmVar, {} ), jsonFormat.format, jsonFormat.indent ) )
+                    .val( JSON.stringify( GM_getValue( gmVar, {} ), null, jsonFormat.indent ) )
                     .on( 'input', function(){ isJson( $( this ), { 'background-color': '#FFF' }, { 'background-color': '#fbcfcf' }, $( '#save_' + id ) ); } )
             ),
             $( '<input type="button" value="Save" id="save_' + id +  '" />' ).click(
@@ -54,6 +64,6 @@ function jsonEditBoxGM( gmVar, jsonFormat={}, hover={} )
                 }
             )
         ]
-
     );
 }
+*/
