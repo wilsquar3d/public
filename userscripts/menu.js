7eacf4199d1304dd23da7c9585a17b0971de2ef7
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2020-03-05 18:55:59 -0500

    Update menu.js

--- userscripts/menu.js
+++ userscripts/menu.js
@@ -15,7 +15,7 @@ var global_defaults =
             menuItemHover: { on: { 'background-color': '#CCC' }, off: { 'background-color': '#FFF' } },
             //jsonEditBox
             jsonEdit: { width: '50%', height: '50%', float: 'left' },
-            jsonText: { width: '100%', height: 'calc(100% - 50px)', 'white-space': 'nowrap' },
+            jsonText: { width: '100%', height: 'calc(100% - 60px)', 'white-space': 'nowrap' },
             editBoxHover: { on: { 'background-color': '#EEE' }, off: { 'background-color': '#FFF' } },
             //isJson
             bgWhite: { 'background-color': '#FFF' },
@@ -108,10 +108,13 @@ function jsonEditBoxGM( gmVar, cssJsonEdit=null, cssJsonText=null, jsonFormat=nu
     ).append(
         [
             $( '<div style="padding:10px;" />' ).append(
-                $( '<textarea id="' + id + '" />' )
-                    .css( cssJsonText )
-                    .val( JSON.stringify( GM_getValue( gmVar, {} ), null, jsonFormat.indent ) )
-                    .on( 'input', function(){ isJson( $( this ), global_defaults.css.bgWhite, global_defaults.css.bgRed, $( '#save_' + id ) ); } )
+                [
+                    $( '<div style="height:10px;font-size:11pt" />' ).val( gmVar ),
+                    $( '<textarea id="' + id + '" />' )
+                        .css( cssJsonText )
+                        .val( JSON.stringify( GM_getValue( gmVar, {} ), null, jsonFormat.indent ) )
+                        .on( 'input', function(){ isJson( $( this ), global_defaults.css.bgWhite, global_defaults.css.bgRed, $( '#save_' + id ) ); } )
+                ]
             ),
             $( '<input type="button" value="Save" id="save_' + id +  '" />' ).click(
                 function()
