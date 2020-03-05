3607f5692fb57fa3ee85a9f031a0debb0a68ca87
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2020-03-05 08:43:37 -0500

    Update utils.js

--- userscripts/utils.js
+++ userscripts/utils.js
@@ -4,3 +4,24 @@ function createID( appID, varName, extras=[] )
 {
     return 'ww_' + appID + '_' + varName + ( extras && extras.length > 0 ? ( '_' + extras.join( '_' ) ) : '' ) + '_ww';
 }
+
+function isJson( elem, pos, neg, btn=null )
+{
+    var test = false;
+    
+    try
+    {
+        JSON.parse( elem.val() );
+        test = true;
+    }
+    catch( e ) {}
+    
+    elem.css( test ? pos : neg );
+    
+    if( btn )
+    {
+        btn.attr( 'disabled', !test );
+    }
+    
+    return test;
+}
