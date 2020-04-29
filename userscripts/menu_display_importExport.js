01b307952f6414cabb21344f5d0edb4961506280
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2020-04-29 12:04:23 -0400

    Create menu_display_importExport.js

---
+++ userscripts/menu_display_importExport.js
@@ -0,0 +1,50 @@
+function display_importExport( id )
+{
+    var dataID = createID( appID, 'importExportData' );
+
+    $( id ).append(
+        $( '<div style="width:100%;height:100%;" />' ).append(
+            [
+                $( '<div style="padding:10px;" />' ).append(
+                    $( '<textarea id="' + dataID + '" style="width:100%;height:calc(100% - 50px);white-space:nowrap;" />' )
+                        .on( 'input change', function(){ isJson( $( this ), global_defaults.css.bgWhite, global_defaults.css.bgRed, $( '#import' ) ) } )
+                ),
+                $( '<input type="button" value="Import" id="import" style="margin-right:10px;" />' ).click(
+                    function()
+                    {
+                        var data = JSON.parse( $( '#' + dataID ).val() );
+                        var keys = Object.keys( data );
+
+                        $.each( keys,
+                            function( ndx, key )
+                            {
+                                GM_setValue( key, data[key] );
+                            });
+                    }
+                ),
+                $( '<input type="button" value="Export" id="export" />' ).click(
+                    function()
+                    {
+                        var data = {};
+
+                        $.each( GM_listValues(),
+                            function( ndx, key )
+                            {
+                                let value = GM_getValue( key, {} );
+                                if( value )
+                                {
+                                    data[key] = value;
+                                }
+                            });
+
+                        $( '#' + dataID ).val( JSON.stringify( data, true, '  ' ) );
+                        $( '#' + dataID ).change();
+                    }
+                )
+            ]
+
+        )
+    );
+
+    $( '#export' ).click();
+}
