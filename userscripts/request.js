af3b3b6765b6849483a1af588180dabe28374296
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2021-03-30 21:02:06 -0400

    Update request.js

--- userscripts/request.js
+++ userscripts/request.js
@@ -1,33 +1,47 @@
 //https://raw.githubusercontent.com/wilsquar3d/public/master/userscripts/request.js
+//userscript must grant: GM_xmlhttpRequest
+
+function httpRequest( url, method, callback, ...args )
+{
+    httpRequestFull( url, method, {}, '', callback, ...args );
+}
 
 //method: GET, POST, PUT, etc.
-//responseType: document, etc.
-//passthrough is anything you want passed to the result methods
-function request( url, method, responseType, func200, funcOther, passthrough )
+//headers: headers object
+//responseType: document, text, arraybuffer, etc.
+//callback: the function to call with the response
+//args: anything you want passed to the result methods
+function httpRequestFull( url, method, headers, responseType, callback, ...args )
 {
     GM_xmlhttpRequest(
         {
             method: method,
             url: url,
+            headers: headers,
             responseType: responseType,
             onload: function( response )
             {
-                if( 200 == response.status )
-                {
-                    func200( response, passthrough );
-                }
-                else
-                {
-                    funcOther( response, passthrough );
-                }
+                callback( response, ...args );
             }
         });
 }
 
-function log( data, passthrough )
+//automatic 200 validation: ex. httpRequest( url, method, validateHttpRequest, callback_func, <other args> );
+function validateHttpRequest( response, callback, ...args )
 {
-    console.log( 'passthrough' );
-    console.log( passthrough );
-    console.log( 'data' );
-    console.log( data );
+    if( 200 == response.status )
+    {
+        callback( response, ...args );
+    }
+    else
+    {
+        httpRequestError( response, ...args );
+    }
+}
+
+function httpRequestError( response, ...args )
+{
+    console.log( 'httpRequestError' );
+    console.log( response );
+    console.log( args );
 }
