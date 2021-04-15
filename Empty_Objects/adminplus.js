f89b21b3c55a580f93c2dc99e7c90613316dfa32
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2021-04-15 13:43:52 -0400

    Update adminplus.js

--- Empty_Objects/adminplus.js
+++ Empty_Objects/adminplus.js
@@ -49,7 +49,42 @@ if( data )
     }
   };
   
-  data.empty_objects.efb: {
-    profiles: {}
+  data.empty_objects.efb = {
+    aircrafts: {
+      "id": -1,
+      "displayName": "",
+      "typeCode": ""
+    },
+    lookups: {
+      "publishDataTypes": [],
+      "otherDataTypes": [],
+      "deviceRegistrationStatuses": [],
+      "releaseStatuses": []
+    },
+    packages: {},
+    profiles: {
+      "id": -1,
+      "displayName": "",
+      "sortOrder": -1,
+      "applicationPluginUuid": "",
+      "applicationName": ""
+    },
+    releases: {
+      "id": -1,
+      "releaseCode": "",
+      "releaseName": "",
+      "releaseDescription": "",
+      "createdDate": "", //YYYY-MM-DDThh:mm:ss+00:00
+      "publishedDate": "",
+      "authorName": "",
+      "releaseStatus": "",
+      "packageIds": "[]",
+      "packageIdList": [],
+      "templateId": -1,
+      "groupName": "",
+      "autoDataTypeIds": "[]",
+      "autoDataTypeIdList": [],
+      "isTestPublished": false
+    }
   };
 }
