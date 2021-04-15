e22e63a868d6e8a741b08b03f620f60d2d694b62
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2021-04-15 13:06:37 -0400

    Update adminplus.js

--- Empty_Objects/adminplus.js
+++ Empty_Objects/adminplus.js
@@ -2,49 +2,54 @@
 
 if( data )
 {
-  data.empty_objects = {
-    nb: {
-      tenant: {
-        "userList": [],
-        "revoked": false,
-        "id": -1,
-        "customerUuid": "",
-        "customerName": "",
-        "customerShortName": "",
-        "salesRep": "",
-        "admin": "",
-        "dispatchPlusCustomerId": "",
-        "bundleIds": [],
-        "datatypeIds": "[]",
-        "datatypeIdList": [],
-        "applicationPluginIds": "[]",
-        "applicationPluginIdList": []
-      },
-      data_types: {
-        "id": -1,
-        "displayName": "",
-        "dataTypeUuid": "",
-        "applicationName": "",
-        "canBePublished": false,
-        "canBeUploaded": false,
-        "sortOrder": -1,
-        "isVisibleOnDevice": false
-      },
-      bundles: {
-        "id": -1,
-        "displayName": "",
-        "bundlePrefix": "",
-        "bundleUuid": ""
-      },
-      plugins: {
-        "id": -1,
-        "displayName": "",
-        "sortOrder": -1,
-        "applicationPluginUuid": "",
-        "applicationName": ""
-      }
+  if( !data.empty_objects )
+  {
+    data.empty_objects = {};
+  }
+  
+  data.empty_objects.nb = {
+    bundles: {
+      "id": -1,
+      "displayName": "",
+      "bundlePrefix": "",
+      "bundleUuid": ""
     },
-    efb: {
-      
+    data_types: {
+      "id": -1,
+      "displayName": "",
+      "dataTypeUuid": "",
+      "applicationName": "",
+      "canBePublished": false,
+      "canBeUploaded": false,
+      "sortOrder": -1,
+      "isVisibleOnDevice": false
+    },
+    plugins: {
+      "id": -1,
+      "displayName": "",
+      "sortOrder": -1,
+      "applicationPluginUuid": "",
+      "applicationName": ""
+    },
+    tenants: {
+      "userList": [],
+      "revoked": false,
+      "id": -1,
+      "customerUuid": "",
+      "customerName": "",
+      "customerShortName": "",
+      "salesRep": "",
+      "admin": "",
+      "dispatchPlusCustomerId": "",
+      "bundleIds": [],
+      "datatypeIds": "[]",
+      "datatypeIdList": [],
+      "applicationPluginIds": "[]",
+      "applicationPluginIdList": []
+    }
+  };
+  
+  data.empty_objects.efb: {
+    profiles: {}
   };
 }
