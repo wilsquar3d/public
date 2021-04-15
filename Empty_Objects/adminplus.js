57e7c951a2c2984ee20e1a4ce0014b718b3f01e8
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2021-04-15 13:04:14 -0400

    Update adminplus.js

--- Empty_Objects/adminplus.js
+++ Empty_Objects/adminplus.js
@@ -1,10 +1,50 @@
 
 
-if( !data )
+if( data )
 {
-  return; //attaches to data global variable
+  data.empty_objects = {
+    nb: {
+      tenant: {
+        "userList": [],
+        "revoked": false,
+        "id": -1,
+        "customerUuid": "",
+        "customerName": "",
+        "customerShortName": "",
+        "salesRep": "",
+        "admin": "",
+        "dispatchPlusCustomerId": "",
+        "bundleIds": [],
+        "datatypeIds": "[]",
+        "datatypeIdList": [],
+        "applicationPluginIds": "[]",
+        "applicationPluginIdList": []
+      },
+      data_types: {
+        "id": -1,
+        "displayName": "",
+        "dataTypeUuid": "",
+        "applicationName": "",
+        "canBePublished": false,
+        "canBeUploaded": false,
+        "sortOrder": -1,
+        "isVisibleOnDevice": false
+      },
+      bundles: {
+        "id": -1,
+        "displayName": "",
+        "bundlePrefix": "",
+        "bundleUuid": ""
+      },
+      plugins: {
+        "id": -1,
+        "displayName": "",
+        "sortOrder": -1,
+        "applicationPluginUuid": "",
+        "applicationName": ""
+      }
+    },
+    efb: {
+      
+  };
 }
-
-data.empty_objects = {
-  
-};
