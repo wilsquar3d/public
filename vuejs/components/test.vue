a44fe6c9350f1f9197383c81150d28bb269ede61
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2021-04-17 19:19:43 -0400

    Create test.vue

---
+++ vuejs/components/test.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+      {{ JSON.stringify( test ) }}
+	</div>
+</template>
+
+<script>
+	module.exports =
+	{
+		props:
+		{
+			test: { type: Object, default: { label: 'test_label', value: 'test_value' } }
+		},
+		methods:
+		{
+		}
+	}
+</script>
+
+<style>
+</style>
