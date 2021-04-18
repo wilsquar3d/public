11dffcd9f7a6a8902eb247000f56116ef809a4d8
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2021-04-18 16:52:42 -0400

    Update menu.vue

--- vuejs/components/menu/menu.vue
+++ vuejs/components/menu/menu.vue
@@ -1,11 +1,19 @@
   
 <template>
-	<div id="menu">
-		<div style="padding: 10px;">
-			<div
-				v-for='menu_item, ndx in menu_items'
-				class='menu_item'
-			>{{menu_item.name}}</div>
+	<div>
+		<div id="menu">
+			<div style="padding: 10px;">
+				<div
+					v-for='menu_item, ndx in menu_items'
+					class='menu_item'
+					@click='setSelected( menu_item )'
+				>{{menu_item.name}}</div>
+			</div>
+		</div>
+		<div id="display">
+			<template v-if='selected_item'>
+				<component :is='selected_item'></component>
+			</template>
 		</div>
 	</div>
 </template>
@@ -13,12 +21,20 @@
 <script>
 	module.exports =
 	{
+		data:
+		{
+			selected_item: ''
+		},
 		props:
 		{
 			menu_items: { type: Array, default: () => [{ name: 'sample' }] }
 		},
 		methods:
 		{
+			setSelected( menu_item )
+			{
+				this.selected_item = menu_item.component;
+			}
 		}
 	}
 </script>
@@ -37,5 +53,14 @@
 		cursor: pointer;
 		margin-bottom: 10px;
 		background-color: #FFF;
+	}
+	#display
+	{
+		height: 100%;
+		width: calc( 100% - 202px );
+		padding: 0px;
+		margin: 0px;
+		border-left: 2px solid rgb(0, 0, 0);
+		float: left;
 	}
 </style>
