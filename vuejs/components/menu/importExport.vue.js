fff85c2e6e7069cce009b66bccb268d701660825
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2021-04-25 16:44:54 -0400

    Update importExport.vue.js

--- vuejs/components/menu/importExport.vue.js
+++ vuejs/components/menu/importExport.vue.js
@@ -4,16 +4,46 @@ var components = components || {};
 
 components.importExport = {
 	template: `
-	<div style="height:100%;width:calc(100% - 5px);">
-		<textarea style="width: 100%; height: calc(100% - 30px); white-space: nowrap; background-color: rgb(255, 255, 255);">{{sample}}</textarea>
-		<input type="button" value="Import" id="import" style="margin-right:10px;">
-		<input type="button" value="Export" id="export">
+	<div class='full'>
+		<textarea class='output' v-model='data'></textarea>
+		<div class='actions'>
+			<input type='button' value='Import' @click='import_data' style='margin-right:10px;' />
+			<input type='button' value='Export' @click='export_data' />
+		</div>
+		
+		<component is='style'>
+			.full
+			{
+				height: 100%;
+				width: 100%;
+			}
+			.output
+			{
+				width: 100%;
+				height: calc( 100% - 35px );
+				white-space: nowrap;
+			}
+			.actions
+			{
+				margin: 5px 0;
+				height: 25px;
+			}
+		</component>
 	</div>
 	`,
-	computed: {
-		sample()
+	data: () => {
+		return {
+			data: ''
+		};
+	},
+	methods: {
+		import_data()
 		{
-			return 'asdf';
+			//import this.data to ?
+		},
+		export_data()
+		{
+			//export ? to this.data
 		}
 	}
 };
