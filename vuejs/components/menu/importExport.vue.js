95e4c690f6d0511408eaf1ea3fa8f11495cfd693
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2021-04-25 09:41:58 -0400

    Create importExport.vue.js

---
+++ vuejs/components/menu/importExport.vue.js
@@ -0,0 +1,17 @@
+var components = components || {};
+
+components.importExport = {
+	template: `
+	<div style="height:100%;width:calc(100% - 5px);">
+		<textarea style="width: 100%; height: calc(100% - 30px); white-space: nowrap; background-color: rgb(255, 255, 255);">{{sample}}</textarea>
+		<input type="button" value="Import" id="import" style="margin-right:10px;">
+		<input type="button" value="Export" id="export">
+	</div>
+	`,
+	computed: {
+		sample()
+		{
+			return 'asdf';
+		}
+	}
+};
