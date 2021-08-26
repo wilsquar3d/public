a6652988cc1afd5902355a28a99472be6bd9de7a
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2021-08-26 19:51:19 -0400

    Create progressBar.vue

---
+++ vuejs/components/ProgressBar/progressBar.vue
@@ -0,0 +1,86 @@
+//https://raw.githubusercontent.com/wilsquar3d/public/master/vuejs/components/ProgressBar/progressBar.vue
+
+<template>
+	<div class="progressBar">
+
+		<div class='progressBarDisplay'>
+			<svg class='progressBarSvg'>
+				<line
+					class='backgroundLine'
+					x1='0'
+					y1='0'
+					x2='100%'
+					y2='0'
+					></line>
+					<line
+					class='progressBarLine'
+					x1='0'
+					y1='0'
+					:x2='progress + "%"'
+					y2='0'
+				></line>
+			</svg>
+		</div>
+
+		<div class='progressBarPercent'>{{progress}}%</div>
+
+		<div
+			v-if='description.length'
+			class='progressBarDesc'
+		>{{description}}</div>
+
+	</div>
+</template>
+
+<script type='module'>
+	module.exports =
+	{
+		props:
+		{
+			progress: {
+				type: [Number, String],
+				default: 0
+			},
+			description: {
+				type: String,
+				default: ''
+			}
+		}
+	}
+</script>
+
+<style scoped>
+.progressBarDisplay
+{
+  width: calc( 100% - 50px );
+  display: inline-block;
+  vertical-align: middle;
+}
+.progressBarSvg
+{
+  height: 10px;
+  width: 100%;
+}
+.backgroundLine
+{
+  stroke: lavender;
+  stroke-width: 10;
+}
+.progressBarLine
+{
+  stroke: #000;
+  stroke-width: 10;
+}
+.progressBarPercent
+{
+  width: 50px;
+  display: inline-block;
+  padding-left: 10px;
+  font-size: 12pt;
+}
+.progressBarDesc
+{
+  font-size: 10pt;
+  color: grey;
+}
+</style>
