379b80c44db1a8db6d1df6f7c99e42bfe9444ec6
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2021-08-26 20:44:47 -0400

    Update progressBar.vue

--- vuejs/components/ProgressBar/progressBar.vue
+++ vuejs/components/ProgressBar/progressBar.vue
@@ -50,37 +50,37 @@
 </script>
 
 <style scoped>
-.progressBarDisplay
-{
-  width: calc( 100% - 50px );
-  display: inline-block;
-  vertical-align: middle;
-}
-.progressBarSvg
-{
-  height: 10px;
-  width: 100%;
-}
-.backgroundLine
-{
-  stroke: lavender;
-  stroke-width: 10;
-}
-.progressBarLine
-{
-  stroke: #000;
-  stroke-width: 10;
-}
-.progressBarPercent
-{
-  width: 50px;
-  display: inline-block;
-  padding-left: 10px;
-  font-size: 12pt;
-}
-.progressBarDesc
-{
-  font-size: 10pt;
-  color: grey;
-}
+	.progressBarDisplay
+	{
+		width: calc( 100% - 50px );
+		display: inline-block;
+		vertical-align: middle;
+	}
+	.progressBarSvg
+	{
+		height: 10px;
+		width: 100%;
+	}
+	.backgroundLine
+	{
+		stroke: lavender;
+		stroke-width: 10;
+	}
+	.progressBarLine
+	{
+		stroke: #000;
+		stroke-width: 10;
+	}
+	.progressBarPercent
+	{
+		width: 50px;
+		display: inline-block;
+		padding-left: 10px;
+		font-size: 12pt;
+	}
+	.progressBarDesc
+	{
+		font-size: 10pt;
+		color: grey;
+	}
 </style>
