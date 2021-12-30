efee9c21f0a57b0a308ca191124e5071b3ad34a7
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2021-12-29 22:07:48 -0500

    Update PokemonGoGymMap.vue

--- vuejs/PokemonGo/PokemonGoGymMap.vue
+++ vuejs/PokemonGo/PokemonGoGymMap.vue
@@ -3,14 +3,14 @@
 <template>
 	<div id='gym-map-wrap'>
 		
-		<!-- map-point-component
+		<component is='map-point-component'
 			v-if='imageLoaded'
 			:height='mapHeight'
 			:latitude='gym.latitude'
 			:longitude='gym.longitude'
 			:icon='iconGym'
 			:iconScale='iconScale'
-		/ -->
+		/>
 
 		<template v-if='gym.url'>
 			<img class='image' :src='gym.url' :alt='gym.url' @load='onImageLoaded' :style='"max-height:" + mapHeight' />
