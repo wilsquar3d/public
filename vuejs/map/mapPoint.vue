6c22ea290db5fbee73d1982f6f8ccf8ba336fa3c
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2021-12-29 20:04:06 -0500

    Create mapPoint.vue

---
+++ vuejs/map/mapPoint.vue
@@ -0,0 +1,123 @@
+//https://raw.githubusercontent.com/wilsquar3d/public/master/vuejs/components/map/mapPoint.vue
+// @require      https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.9.0/build/ol.js
+// @resource     https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.9.0/css/ol.css
+
+<template>
+    <div class='map-wrap' :style='"height:" + height'>
+        <div :id='uniqueId' class='map' ref='map'></div>
+	</div>
+</template>
+
+<script>
+    module.exports =
+    {
+        data: () => {
+            return {
+                map: null,
+                style: null,
+                layerMarkers: null
+            };
+        },
+        props:
+        {
+            height: { type: String, default: '100vh' },
+            latitude: { type: [Number, String], required: true },
+            longitude: { type: [Number, String], required: true },
+            zoom: { type: Number, default: 15 },
+            markers: { type: Array, default: [] }, //TODO validate lat/lng list
+            icon: { type: String, default: null },
+            iconScale: { type: Number, default: 1 }
+        },
+        mounted()
+        {
+            this.createMap();
+            this.createStyle();
+            this.createMarkersLayer();
+            this.createCenterMarker();
+        },
+        methods:
+        {
+            createMap()
+            {
+                this.map = new ol.Map( {
+                    target: this.uniqueId,
+                    layers: [ new ol.layer.Tile( { source: new ol.source.OSM() } ) ],
+                    view: new ol.View( {
+                        center: this.center,
+                        zoom: this.zoom
+                    } )
+                } );
+            },
+            createStyle()
+            {
+                if( this.icon )
+                {
+                    this.style = new ol.style.Style( {
+                        image: new ol.style.Icon( {
+                            anchor: [0, 0],
+                            src: this.icon,
+                            scale: this.iconScale
+                        } ),
+                    } );
+                }
+            },
+            createMarkersLayer()
+            {
+                this.layerMarkers = new ol.layer.Vector( {
+                    source: new ol.source.Vector( {
+                        features: [],
+                        style: this.style
+                    } )
+                } );
+
+                this.map.addLayer( this.layerMarkers );
+            },
+            createCenterMarker()
+            {
+                var marker = new ol.Feature( {
+                    geometry: new ol.geom.Point( this.center ),
+                } );
+
+                if( this.style )
+                {
+                    marker.setStyle( this.style );
+                }
+
+                this.addMarkerToMap( marker );
+            },
+            addMarkerToMap( marker )
+            {
+                this.layerMarkers.getSource().addFeature( marker );
+            }
+        },
+        computed:
+        {
+            uniqueId()
+            {
+                return ( 'map-wrap_' + this.latitude + '_' + this.longitude ).replaceAll( '.', '' );
+            },
+            center()
+            {
+                return ol.proj.fromLonLat( [ this.longitude, this.latitude ] );
+            }
+        }
+    }
+</script>
+
+<style>
+    .map-wrap
+    {
+        width: 100%;
+        border: 1px solid #000;
+    }
+    .map
+    {
+        width: 100%;
+        height: 100%;
+    }
+    .ol-attribution
+    {
+        display: none;
+    }
+</style>
+    
