b3deb02d83b6b6b7d9ac8e7d5b7bdb3e870f1707
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2021-12-29 20:23:54 -0500

    Update mapPoint.vue

--- vuejs/map/mapPoint.vue
+++ vuejs/map/mapPoint.vue
@@ -39,10 +39,10 @@
         {
             createMap()
             {
-                this.map = new ol.Map( {
+                this.map = new $ol.Map( {
                     target: this.uniqueId,
-                    layers: [ new ol.layer.Tile( { source: new ol.source.OSM() } ) ],
-                    view: new ol.View( {
+                    layers: [ new $ol.layer.Tile( { source: new $ol.source.OSM() } ) ],
+                    view: new $ol.View( {
                         center: this.center,
                         zoom: this.zoom
                     } )
@@ -52,8 +52,8 @@
             {
                 if( this.icon )
                 {
-                    this.style = new ol.style.Style( {
-                        image: new ol.style.Icon( {
+                    this.style = new $ol.style.Style( {
+                        image: new $ol.style.Icon( {
                             anchor: [0, 0],
                             src: this.icon,
                             scale: this.iconScale
@@ -63,8 +63,8 @@
             },
             createMarkersLayer()
             {
-                this.layerMarkers = new ol.layer.Vector( {
-                    source: new ol.source.Vector( {
+                this.layerMarkers = new $ol.layer.Vector( {
+                    source: new $ol.source.Vector( {
                         features: [],
                         style: this.style
                     } )
@@ -74,8 +74,8 @@
             },
             createCenterMarker()
             {
-                var marker = new ol.Feature( {
-                    geometry: new ol.geom.Point( this.center ),
+                var marker = new $ol.Feature( {
+                    geometry: new $ol.geom.Point( this.center ),
                 } );
 
                 if( this.style )
@@ -98,7 +98,7 @@
             },
             center()
             {
-                return ol.proj.fromLonLat( [ this.longitude, this.latitude ] );
+                return $ol.proj.fromLonLat( [ this.longitude, this.latitude ] );
             }
         }
     }
