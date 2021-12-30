3915f68efee473a665b5af978b33b5a73f1eef8c
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2021-12-30 10:40:56 -0500

    Update PokemonGoGymMap.vue

--- vuejs/PokemonGo/PokemonGoGymMap.vue
+++ vuejs/PokemonGo/PokemonGoGymMap.vue
@@ -35,6 +35,10 @@
         {
             gym: { type: Object, required: true }
         },
+	created()
+	{
+		console.log(Vue.options.components || "unknown");
+	},
         methods:
         {
             onImageLoaded()
