922096f5a5dc772b401dbe5603f62b6020bd1e1f
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2022-01-08 20:26:23 -0500

    Update genericLoader.vue.js

--- vuejs/components/genericLoader.vue.js
+++ vuejs/components/genericLoader.vue.js
@@ -1,4 +1,4 @@
-//https://raw.githubusercontent.com/wilsquar3d/public/master/vuejs/components/GenericLoader.vue.js
+//https://raw.githubusercontent.com/wilsquar3d/public/master/vuejs/components/genericLoader.vue.js
 
 function generic_load_component( component_suffix=null )
 {
