65d852cd31cf3ecb26b1adfcaad10ca3df705327
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2022-11-08 09:52:07 -0500

    Create download.js

---
+++ userscripts/download.js
@@ -0,0 +1,33 @@
+
+
+function autoDownload( filename, url, type='image/png' )
+{
+    GM_xmlhttpRequest(
+        {
+            method: 'GET',
+            url: url,
+            responseType: 'blob',
+            onload: function( response )
+            {
+                if( 200 == response.status )
+                {
+                    var data = new Blob( [new Uint8Array( response.responseText.split( '' ).map( ch => ch.charCodeAt( 0 ) ) )], { type: type } );
+                    var fileURL = window.URL.createObjectURL( data );
+
+                    const link = document.createElement( 'a' );
+                    link.href = fileURL;
+                    link.setAttribute( 'download', filename );
+                    document.body.appendChild( link );
+                    link.click();
+                    link.remove();
+
+                    window.URL.revokeObjectURL( fileURL );
+                }
+                else
+                {
+                    console.log( response );
+                }
+            }
+        }
+    );
+}
