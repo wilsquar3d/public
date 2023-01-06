633a8d886bad256def28b0cf099bc5eb0d01c131
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2023-01-05 20:30:29 -0500

    Update menu.vue

--- vuejs/components/menu/menu.vue
+++ vuejs/components/menu/menu.vue
@@ -1,15 +1,34 @@
 //https://raw.githubusercontent.com/wilsquar3d/public/master/vuejs/components/menu/menu.vue
+//custom menu width?
 
 <template>
 	<div id='wrapper'>
 		<div id='menu'>
 			<div class='menu_item_wrapper'>
-				<div
-					v-for='menu_item, ndx in menu_items'
-					class='menu_item'
-					:class='menu_item == selected_item ? "menu_item_selected" : "menu_item_unselected"'
-					@click='setSelected( menu_item )'
-				>{{menu_item.name}}</div>
+				<template v-for='menu_item, ndx in menu_items'>
+					<template v-if='menu_item.type'>
+						<div v-if='"divider" == menu_item.type && menu_item.name'
+							class='menu_item_divider_wrapper'
+						>
+							<hr class='menu_item_label_divider' />
+							<span class='menu_item_divier_label'>{{menu_item.name}}</span>
+							<hr class='menu_item_label_divider' />
+						</div>
+						<div v-else-if='"divider" == menu_item.type'
+							class='menu_item_divider_wrapper'
+						>
+							<hr class='menu_item_divider' />
+						</div>
+					</template>
+					<template v-else>
+						<div
+							class='menu_item'
+							:class='menu_item == selected_item ? "menu_item_selected" : "menu_item_unselected"'
+							@click='setSelected( menu_item )'
+							:key='menu_item.name'
+						>{{menu_item.name}}</div>
+					</template>
+				</template>
 			</div>
 		</div>
 		<div id='display_wrapper'>
@@ -60,7 +79,7 @@
 	#menu
 	{
 		height: 100%;
-		width: 200px;
+		width: 250px;
 		padding: 0px;
 		margin: 0px;
 		float: left;
@@ -91,7 +110,7 @@
 	#display_wrapper
 	{
 		height: 100%;
-		width: calc( 100% - 202px );
+		width: calc( 100% - 252px );
 		padding: 0px;
 		margin: 0px;
 		border-left: 2px solid rgb(0, 0, 0);
@@ -103,5 +122,25 @@
 		margin: 10px;
 		height: calc( 100% - 20px );
 		overflow-y: auto;
+	}
+	.menu_item_divider_wrapper
+	{
+		padding: 0 10px;
+	}
+	.menu_item_divider
+	{
+		background-color: #CCC;
+	}
+	.menu_item_label_divider
+	{
+		background-color: #CCC;
+		display: inline-block;
+		width: 20px;
+		vertical-align: middle;
+	}
+	.menu_item_divider_label
+	{
+		padding: 5px;
+		font-size: 80%;
 	}
 </style>
