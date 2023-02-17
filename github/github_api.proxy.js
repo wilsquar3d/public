139f0a6bf1bb205eb4eb20f84b24cfd98f6da592
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2023-02-16 21:11:59 -0500

    Create github_api.proxy.js

---
+++ github/github_api.proxy.js
@@ -0,0 +1,103 @@
+//https://raw.githubusercontent.com/wilsquar3d/public/master/github/github_api.proxy.js
+// requires request.js
+
+/*
+// usage example
+let token = 'github_pat_<token>';
+let repo = '<REPO>';
+let path = '<PATH>/<FILE.EXE>';
+let content = '<FILE CONTENT>';
+
+github_api.setConfig( token, repo, path );
+console.log( await github_api.sha() );
+console.log( await github_api.get() );
+console.log( await github_api.getManual( token, repo, path ) );
+console.log( await github_api.put( content ) );
+console.log( await github_api.putManual( token, repo, path, content ) );
+*/
+
+var github_api = {
+    proxy_url: 'http://localhost:8000/ProxyServer.py',
+    owner: 'wilsquar3d',
+    config: {
+        token: '',
+        repo: '',
+        path: ''
+    },
+
+    setProxyUrl: function( url ) {
+        this.proxy_url = url;
+    },
+    setOwner: function( owner ) {
+        this.owner = owner;
+    },
+    setConfig: function( token, repo, path ) {
+        this.config.token = token;
+        this.config.repo = repo;
+        this.config.path = path;
+    },
+
+    url: function()
+    {
+        return `https://api.github.com/repos/${this.owner}/${this.config.repo}/contents/${this.config.path}`;
+    },
+    headers: function() {
+        return {
+            Authorization: `Bearer ${this.config.token}`,
+            Accept: 'application/vnd.github+json',
+            'X-GitHub-Api-Version': '2022-11-28'
+        };
+    },
+    payload: function( commit_data, sha, msg='' ) {
+        return {
+            message: msg,
+            content: btoa( commit_data ),
+            sha: sha
+        };
+    },
+
+    get: async function() {
+        let request = buildRequest( this.url(), 'GET', this.headers(), '', 'application/json' );
+        let proxy_request = buildRequest( this.proxy_url, 'POST', { 'Content-Type': 'application/json' }, request, '' );
+
+        return await httpRequest( proxy_request );
+    },
+
+    getManual: async function( token, repo, path ) {
+        let config = JSON.stringify( this.config );
+
+        this.setConfig( token, repo, path );
+        let response = this.get();
+
+        this.config = JSON.parse( config );
+
+        return response;
+    },
+
+    put: async function( commit_data, msg='' ) {
+        let sha = await this.sha();
+        let request = buildRequest( this.url(), 'PUT', this.headers(), this.payload( commit_data, sha, msg ), 'application/json' );
+        let proxy_request = buildRequest( this.proxy_url, 'POST', { 'Content-Type': 'application/json' }, request, '' );
+
+        return await httpRequest( proxy_request );
+    },
+
+    putManual: async function( token, repo, path, commit_data, msg='' ) {
+        let config = JSON.stringify( this.config );
+
+        this.setConfig( token, repo, path );
+        let response = this.put( commit_data, msg );
+
+        this.config = JSON.parse( config );
+
+        return response;
+    },
+
+    sha: async function() {
+        let result = await this.get();
+        let response = JSON.parse( result.response );
+        let content = JSON.parse( response.content );
+
+        return content.sha;
+    }
+};
