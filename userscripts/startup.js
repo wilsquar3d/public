2a5a1de50df65ca0c712591f9f19b0218a238fb7
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2023-03-17 20:55:17 -0400

    Update startup.js

--- userscripts/startup.js
+++ userscripts/startup.js
@@ -124,6 +124,12 @@ var startup = {
 };
 
 /*
+let gmLoad = new GM_Data( 'test' );
+gmLoad.setAutoReload( false );
+gmLoad.load();
+console.log( gmLoad );
+
+let gmData = new GM_Data( 'test' );
 gmData.setAutoReload( false );
 gmData.set( { a: [1,2,3,4,5], b: { c: 'C', d: 'D', e: { f: 7 } } } );
 gmData.setSub( 'dddd', 'b', 'd' );
@@ -135,48 +141,77 @@ gmData.get().a.push( 6 );
 gmData.setFixedIds( 'b' );
 gmData.get().z = 'Z';
 console.log( gmData );
+gmData.save();
+
+{
+    "a": [1,2,3,4,5,6],
+    "b": {
+        "d": "dddd",
+        "e": {
+            "f": {
+                "g": "G",
+                "h": 7
+            }
+        },
+        "z": "Z"
+    }, "aa": ["a","a"]
+}
 */
-var gmData = {
-    data: {},
-    fixedIds: [],
-    defaultValue: {},
-    lastUpdated: true,
-    lastUpdatedKey: 'last_update',
-    autoReload: true,
+class GM_Data
+{
+    id = null;
+    data = {};
+    defaultValue = {};
+    fixedIds = [];
+    lastUpdated = false;
+    lastUpdatedKey = 'last_update';
+    autoReload = false;
+
+    constructor( id, lastUpdated=false, autoReload=false )
+    {
+        this.id = id || startup.appID || 'data';
+        this.lastUpdated = lastUpdated;
+        this.autoReload = autoReload;
+    }
 
     setFixedIds( ...ids )
     {
         this.fixedIds = ids.flat();
-    },
+    }
     setDefaultValue( val )
     {
         this.defaultValue = val;
-    },
+    }
     setLastUpdated( val )
     {
         this.lastUpdated = val;
-    },
+    }
     setLastUpdatedKey( val )
     {
         this.lastUpdatedKey = val;
-    },
+    }
     setAutoReload( val )
     {
         this.autoReload = val;
-    },
+    }
 
-    load: function( default_value )
+    load( default_value )
     {
         let val = default_value || this.defaultValue;
 
-        this.data = GM_getValue( startup.appID, val ) || val;
-    },
+        this.data = GM_getValue( this.id, val ) || val;
+    }
 
-    get: function( ...ids )
+    clear()
+    {
+        this.data = copyJson( this.defaultValue );
+    }
+
+    get( ...ids )
     {
         return this.getDefault( this.defaultValue, ...ids );
-    },
-    getSub: function( ...ids )
+    }
+    getSub( ...ids )
     {
         if( !ids.length )
         {
@@ -186,8 +221,8 @@ var gmData = {
         let last = ids.pop();
 
         return [last, this.getDefault( this.defaultValue, ...ids )];
-    },
-    getDefault: function( default_value, ...ids )
+    }
+    getDefault( default_value, ...ids )
     {
         if( this.autoReload )
         {
@@ -213,13 +248,13 @@ var gmData = {
         }
 
         return dataLoad;
-    },
+    }
 
-    set: function( value )
+    set( value )
     {
         this.data = value;
-    },
-    setSub: function( value, ...ids )
+    }
+    setSub( value, ...ids )
     {
         let [last, dataLoad] = this.getSub( ...ids );
 
@@ -227,10 +262,12 @@ var gmData = {
         {
             dataLoad[last] = value;
         }
-    },
+    }
 
-    save: function( dataSave )
+    save( dataSave )
     {
+        dataSave = dataSave || this.data;
+
         if( this.lastUpdated )
         {
             dataSave[this.lastUpdatedKey] = Date.now();
@@ -238,12 +275,12 @@ var gmData = {
 
         dataSave = dataSave || this.data;
 
-        GM_setValue( startup.appID, dataSave );
+        GM_setValue( this.id, dataSave );
 
         this.data = dataSave;
-    },
+    }
 
-    delete: function( ...ids )
+    delete( ...ids )
     {
         let [last, dataLoad] = this.getSub( ...ids );
 
@@ -251,11 +288,11 @@ var gmData = {
         {
             delete dataLoad[last];
         }
-    },
+    }
 
-    prettyLastUpdated( data )
+    static prettyLastUpdated( data )
     {
-        let dataLoad = data || get();
+        let dataLoad = data || this.get();
 
         if( Object.keys( dataLoad ).includes( this.lastUpdatedKey ) )
         {
@@ -264,4 +301,113 @@ var gmData = {
 
         return null;
     }
-};
+}
+
+/*
+let gmNewData = new GM_NewData( 'data' );
+gmNewData.setAutoReload( false );
+gmNewData.clear();
+gmNewData.add( 'a', 'A' );
+gmNewData.add( 'a', 'B' );
+gmNewData.add( 'foo', 'bar' );
+gmNewData.merge();
+gmNewData.add( 'a', 'C' );
+console.log( gmNewData );
+gmNewData.save();
+*/
+
+class GM_NewData
+{
+    data = null;
+    newData = null;
+
+    constructor( id, lastUpdated=false, autoReload=false )
+    {
+        let dataId = id || startup.appID || 'data';
+        let newDataId = dataId + '_new';
+
+        this.data = new GM_Data( dataId, lastUpdated, autoReload );
+        this.newData = new GM_Data( newDataId, lastUpdated, autoReload );
+    }
+
+    setFixedIds( ...ids )
+    {
+        this.data.fixedIds = ids.flat();
+        this.newData.fixedIds = ids.flat();
+    }
+    setDefaultValue( val )
+    {
+        this.data.defaultValue = val;
+        this.newData.defaultValue = val;
+    }
+    setLastUpdated( val )
+    {
+        this.data.lastUpdated = val;
+        this.newData.lastUpdated = val;
+    }
+    setLastUpdatedKey( val )
+    {
+        this.data.lastUpdatedKey = val;
+        this.newData.lastUpdatedKey = val;
+    }
+    setAutoReload( val )
+    {
+        this.data.autoReload = val;
+        this.newData.autoReload = val;
+    }
+
+    load()
+    {
+        this.data.load();
+        this.newData.load();
+    }
+
+    clear()
+    {
+        this.data.clear();
+        this.newData.clear();
+    }
+
+    add( key, value, compareFunc=null )
+    {
+        let update = true;
+        let val = this.data.getDefault( null, key );
+
+        if( val )
+        {
+            update = compareFunc
+                ? compareFunc( val, value, key )
+                : GM_NewData.defaultCompareFunc( val, value, key );
+        }
+
+        if( update )
+        {
+            this.newData.setSub( value, key );
+        }
+    }
+
+    merge()
+    {
+        let data = this.newData.get();
+
+        for( const key in data )
+        {
+            this.data.setSub( data[key], key );
+        }
+
+        this.newData.clear();
+    }
+
+    save()
+    {
+        this.data.save();
+        this.newData.save();
+    }
+
+    //////////////////////
+
+    static defaultCompareFunc( left, right, key )
+    {
+        return left != right;
+    }
+}
