468785f74916f0b3e9ae030f1914adfc36b92996
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2023-03-23 14:49:18 -0400

    Update styleHelper.js

--- userscripts/styleHelper.js
+++ userscripts/styleHelper.js
@@ -1,12 +1,54 @@
-// https://raw.githubusercontent.com/wilsquar3d/public/master/userscripts/styleHelper.js
+// https://raw.githubusercontent.com/wilsquar3d/public/master/userscripts/StyleHelper.js
+// @require      https://raw.githubusercontent.com/wilsquar3d/public/master/userscripts/utils.js
 // @grant        GM_addStyle
 
 class StyleHelper
 {
-    static blink( loc, className='blinking', style='background-color: red !important;', interval=500 )
+    static content( qry )
+    {
+        return $( qry ).val() || $( qry ).text();
+    }
+
+    // wrapper to return the element after setting text
+    static setText( qry, text )
+    {
+        let elem = $( qry );
+        elem.text( text );
+
+        return elem;
+    }
+
+    static blink( qry, className='blinking', style='background-color: red !important;', interval=500 )
     {
         GM_addStyle( `.${className} { ${style} }` );
 
-        setInterval( function() { $( loc ).toggleClass( className ); }, interval );
+        setInterval( function() { $( qry ).toggleClass( className ); }, interval );
+    }
+
+    // src can be query or element
+    // copy can be query or element
+    static copyToClipboard( src, copy )
+    {
+        let elem = $( src );
+        elem.click( () => navigator.clipboard.writeText( StyleHelper.content( copy ) ) );
+
+        return elem;
+    }
+
+    // content can be text or element
+    // copy can be query or element
+    static copyToClipboardLink( content, copy, className='' )
+    {
+        let elem = StyleHelper.link( content, '#', className )
+        StyleHelper.copyToClipboard( elem, copy );
+
+        return elem;
+    }
+
+    static link( content, href, className='' )
+    {
+        return isString( content )
+            ? $( `<a href='${href}' class='${className}'>${content}</a>` )
+            : $( `<a href='${href}' class='${className}'></a>` ).append( $( content ) );
     }
 }
