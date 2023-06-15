1b0825044047ed8e91cf79015748ab49d9d93291
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2023-06-14 21:07:57 -0400

    Update proxy.js

--- userscripts/proxy.js
+++ userscripts/proxy.js
@@ -2,14 +2,17 @@
 // userscript must grant: GM_xmlhttpRequest
 // requires request.js
 
-class Proxy
+unsafeWindow.gm_version = unsafeWindow.gm_version || {};
+unsafeWindow.gm_version.proxy = { "version": "1.0.0", "source": "https://raw.githubusercontent.com/wilsquar3d/public/master/userscripts/proxy.js" };
+
+class ProxyServer
 {
     static url = 'http://localhost:8000/ProxyServer.py';
 
     static request( url, type, headers={}, payload={}, responseType='application/json' )
     {
         let request = buildRequest( url, type, headers, payload, responseType );
-        let proxy_request = buildRequest( Proxy.url, 'POST', { 'Content-Type': 'application/json' }, request, '' );
+        let proxy_request = buildRequest( ProxyServer.url, 'POST', { 'Content-Type': 'application/json' }, request, '' );
 
         return proxy_request;
     }
