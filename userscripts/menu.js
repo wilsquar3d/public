317a31d19fc528b2181ef998d4ee053387c96067
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2023-06-18 13:08:32 -0400

    Update menu.js

--- userscripts/menu.js
+++ userscripts/menu.js
@@ -2,7 +2,7 @@
 //requires utils.js
 
 unsafeWindow.gm_version = unsafeWindow.gm_version || {};
-unsafeWindow.gm_version.menu = { "version": "1.0.0", "source": "https://raw.githubusercontent.com/wilsquar3d/public/master/userscripts/menu.js" };
+unsafeWindow.gm_version.menu = { "version": "1.1.0", "source": "https://raw.githubusercontent.com/wilsquar3d/public/master/userscripts/menu.js" };
 
 var global_defaults =
     {
@@ -30,9 +30,9 @@ var global_defaults =
         }
     };
 
-function createMenu2( menuItems, props={} )
+function createMenu( menuItems=[], props={ startPage: null } )
 {
-    createMenu( menuItems, props.displayCSS, props.menuItemsCSS, props.menuCSS, props.bodyCSS );
+    createMenuHelper( menuItems, props );
 
     let pageParts = window.location.href.split( 'startPage=' );
 
@@ -50,27 +50,12 @@ function createMenu2( menuItems, props={} )
     }
 }
 
-function createMenu( menuItems=[], displayCSS=null, menuItemsCSS=null, menuCSS=null, bodyCSS=null )
+function createMenuHelper( menuItems=[], props={ displayCSS: null, menuItemsCSS: null, menuCSS: null, bodyCSS: null } )
 {
-    if( !displayCSS )
-    {
-        displayCSS = global_defaults.css.display;
-    }
-
-    if( !menuItemsCSS )
-    {
-        menuItemsCSS = global_defaults.css.menuItems;
-    }
-
-    if( !menuCSS )
-    {
-        menuCSS = global_defaults.css.menu;
-    }
-
-    if( !bodyCSS )
-    {
-        bodyCSS = global_defaults.css.body;
-    }
+    let displayCSS = props.displayCSS || global_defaults.css.display;
+    let menuItemsCSS = props.menuItemsCSS || global_defaults.css.menuItems;
+    let menuCSS = props.menuCSS || global_defaults.css.menu;
+    let bodyCSS = props.bodyCSS || global_defaults.css.body;
 
     $( 'body' ).html( '' ).css( bodyCSS );
     $( 'body' ).append(
@@ -83,22 +68,15 @@ function createMenu( menuItems=[], displayCSS=null, menuItemsCSS=null, menuCSS=n
 
 //displayFunc will accept the selector to attach to
 //displayFunc can be a function() or object { func: function(), props: [props]]
-function menuItem( name, displayFunc, css=null, hover=null )
+function menuItem( name, displayFunc, props={ css: null, hover: null } )
 {
-    if( !css )
-    {
-        css = global_defaults.css.menuItem;
-    }
+    let css = props.css || global_defaults.css.menuItem;
+    let hover = props.hover || global_defaults.css.menuItemHover;
 
-    if( !hover )
-    {
-        hover = global_defaults.css.menuItemHover;
-    }
-
-    let props = [];
+    let funcProps = [];
     if( isObject( displayFunc ) )
     {
-        props = Array.isArray( displayFunc.props ) ? displayFunc.props : [displayFunc.props];
+        funcProps = Array.isArray( displayFunc.props ) ? displayFunc.props : [displayFunc.props];
         displayFunc = displayFunc.func;
     }
 
@@ -107,32 +85,16 @@ function menuItem( name, displayFunc, css=null, hover=null )
         .hover( function(){ $( this ).css( hover.on ); }, function(){ $( this ).css( hover.off ); } )
         .text( name )
         .attr( 'id', createSafeID( name ) )
-        .click( function(){ $( '#display' ).html( '' ); displayFunc( '#display', ...props ); } );
+        .click( function(){ $( '#display' ).html( '' ); displayFunc( '#display', ...funcProps ); } );
 }
 
-function jsonEditBoxGM( gmVar, cssJsonEdit=null, cssJsonText=null, jsonFormat=null, hover=null )
+function jsonEditBoxGM( gmVar, props={ cssJsonEdit: null, cssJsonText: null, jsonFormat: null, hover: null } )
 {
-    if( !cssJsonEdit )
-    {
-        cssJsonEdit = global_defaults.css.jsonEdit;
-    }
-
-    if( !cssJsonText )
-    {
-        cssJsonText = global_defaults.css.jsonText;
-    }
-
-    if( !jsonFormat )
-    {
-        jsonFormat = global_defaults.json.settings;
-    }
-
-    if( !hover )
-    {
-        hover = global_defaults.css.editBoxHover;
-    }
-
-    var id = gmVar; //TODO should sanitize or randomize
+    let cssJsonEdit = props.cssJsonEdit || global_defaults.css.jsonEdit;
+    let cssJsonText = props.cssJsonText || global_defaults.css.jsonText;
+    let jsonFormat = props.jsonFormat || global_defaults.json.settings;
+    let hover = props.hover || global_defaults.css.editBoxHover;
+    let id = gmVar; //TODO should sanitize or randomize
 
     return $( '<div />' ).css( cssJsonEdit ).hover(
         function(){ $( this ).css( hover.on ); },
