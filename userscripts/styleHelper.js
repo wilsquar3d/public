007a57d31edf36b9c2386e2fc8a952241428754e
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2023-08-13 20:51:10 -0400

    Update styleHelper.js

--- userscripts/styleHelper.js
+++ userscripts/styleHelper.js
@@ -3,8 +3,34 @@
 // @require      https://raw.githubusercontent.com/wilsquar3d/public/master/scripts/jqueryHelper.js
 // @grant        GM_addStyle
 
+
+unsafeWindow.gm_version = unsafeWindow.gm_version || {};
+unsafeWindow.gm_version.colour = { "version": "1.0.0", "source": "https://raw.githubusercontent.com/wilsquar3d/public/master/userscripts/StyleHelper.js" };
+
 class StyleHelper
 {
+    // input can be query or style to be tested
+    static computedStyle( input )
+    {
+        let style = null;
+        let [valid, elem] = JQueryHelper.isValidSelector( input );
+
+        if( valid )
+        {
+            style = JSON.parse( JSON.stringify( window.getComputedStyle( document.body.appendChild( elem[0] ) ) ) );
+        }
+        else
+        {
+            let id = 'id_' + guid();
+            let temp = $( `<div id='${id}' style='${input}' />` );
+            style = JSON.parse( JSON.stringify( window.getComputedStyle( document.body.appendChild( temp[0] ) ) ) );
+
+            $( '#' + id ).remove();
+        }
+
+        return style;
+    }
+
     // qry can be query, element or string
     static content( qry )
     {
@@ -49,6 +75,53 @@ class StyleHelper
         StyleHelper.copyToClipboard( elem, copy );
 
         return elem;
+    }
+
+    // content can be text or element
+    // click swaps className to className2 for delay amount of time
+    static feedbackLinkBase( content, className='', style='', className2='', style2='', delay=1000 )
+    {
+        GM_addStyle( `.${className} { ${style} }` );
+
+        let elem = StyleHelper.link( content, '#', className );
+
+        if( className2 && style2 )
+        {
+            GM_addStyle( `.${className2} { ${style2} }` );
+
+            elem.click(
+                function()
+                {
+                    $( elem ).removeClass( className ).addClass( className2 );
+
+                    setTimeout(
+                        function()
+                        {
+                            $( elem ).removeClass( className2 ).addClass( className );
+                        },
+                        delay
+                    );
+                }
+            );
+        }
+
+        return elem;
+    }
+
+    static feedbackLink( content, style='color: black !important; background-color: white;', style2='color: white !important; background-color: black;' )
+    {
+        return StyleHelper.feedbackLinkBase( content, 'class_' + guid(), style, 'class_' + guid(), style2 );
+    }
+
+    static invertFeedbackLink( content, style='', delay=1000 )
+    {
+        let tempStyle = StyleHelper.computedStyle( style );
+
+        let color = style ? tempStyle.color.toUpperCase() : '00F';
+        let bground = tempStyle.backgroundColor.toUpperCase();
+        let invertStyle = `color: ${Colour.invert( color )} !important; background-color: ${Colour.invert( bground )};`;
+
+        return StyleHelper.feedbackLinkBase( content, 'class_' + guid(), style, 'class_' + guid(), invertStyle, delay );
     }
 
     static link( content, href, className='' )
