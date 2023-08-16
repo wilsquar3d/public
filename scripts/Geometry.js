f13fbb4cddf26c69de95324a176102f60916652f
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2023-08-15 20:03:16 -0400

    Create Geometry.js

---
+++ scripts/Geometry.js
@@ -0,0 +1,13 @@
+//https://raw.githubusercontent.com/wilsquar3d/public/master/scripts/Geometry.js
+
+class Point
+{
+    x;
+    y;
+
+    constructor( x, y )
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
