69960a2b6583a27d5af487e8fd91754542034462
WilSquar3d <31278195+wilsquar3d@users.noreply.github.com>
2024-08-31 20:49:33 -0400

    Create window.js

---
+++ userscripts/window.js
@@ -0,0 +1,29 @@
+// https://raw.githubusercontent.com/wilsquar3d/public/master/userscripts/window.js
+
+function loadWindowAndClose( url, maxRetries=10, interval=500 )
+{
+    return new Promise(
+        function( resolve, reject )
+        {
+            let win = window.open( url );
+
+            const id = setInterval( () => {
+                if( win.window.length )
+                {
+                    win.close();
+                }
+
+                if( win.closed )
+                {
+                    clearInterval( id );
+                    resolve();
+                }
+
+                if( --maxRetries <= 0 )
+                {
+                    reject( `Maximum number of retries '${maxRetries}' exceeded!` );
+                }
+            }, interval );
+        }
+    );
+}
